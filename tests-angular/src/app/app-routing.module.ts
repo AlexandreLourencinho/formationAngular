65e6b94e853dca4bde71255ebb58419d6438b624
Alexandre <alexandre.lourencinho@gmail.com>
2022-06-23 16:05:57 +0200

    fix: adding modules, routes and children routes

--- tests-angular/src/app/app-routing.module.ts
+++ tests-angular/src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {CustomersComponent} from "./customers/customers.component";
-import {ProductsComponent} from "./products/products.component";
+import {CustomersComponent} from "./components/customers/customers.component";
+import {ProductsComponent} from "./components/products/products.component";
+import {LoginComponent} from "./components/login/login.component";
+import {AdminTemplateComponent} from "./components/admin-template/admin-template.component";
+import {AuthenticationGuard} from "./guards/authentication.guard";
+import {NewProductComponent} from "./components/new-product/new-product.component";
+import {EditProductsComponent} from "./components/edit-products/edit-products.component";
 
 const routes: Routes = [
-  { path : "customers", component : CustomersComponent},
-  { path : "products", component : ProductsComponent},
+  { path : "login", component : LoginComponent},
+  { path : "", component : LoginComponent},
+  { path : "admin", component : AdminTemplateComponent,canActivate: [AuthenticationGuard], children : [
+      { path : "customers", component : CustomersComponent},
+      { path : "products", component : ProductsComponent},
+      { path : "new-product", component : NewProductComponent},
+      { path : "edit-product/:id", component : EditProductsComponent},
+    ]},
+
+
 ];
 
 @NgModule({
--- tests-angular/src/app/app.module.ts
+++ tests-angular/src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductsComponent } from './products/products.component';
-import { CustomersComponent } from './customers/customers.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CustomersComponent } from './components/customers/customers.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import { LoginComponent } from './components/login/login.component';
+import { AdminTemplateComponent } from './components/admin-template/admin-template.component';
+import { NewProductComponent } from './components/new-product/new-product.component';
+import { EditProductsComponent } from './components/edit-products/edit-products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    CustomersComponent
+    CustomersComponent,
+    LoginComponent,
+    AdminTemplateComponent,
+    NewProductComponent,
+    EditProductsComponent
   ],
   imports: [
     BrowserModule,
