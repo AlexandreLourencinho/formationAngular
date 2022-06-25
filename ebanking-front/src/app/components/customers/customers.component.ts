51b772fe042cc621e4b764f4ee2fe6b59871b0f3
Alexandre <alexandre.lourencinho@gmail.com>
2022-06-25 16:30:21 +0200

    feat: customers component

---
+++ ebanking-front/src/app/components/customers/customers.component.ts
@@ -0,0 +1,69 @@
+import {Component, OnInit} from '@angular/core';
+import {CustomersService} from "../../services/customers/customers.service";
+import {Customer} from "../../model/Customers.model";
+import {catchError, map, Observable, throwError} from "rxjs";
+import {FormBuilder, FormGroup} from "@angular/forms";
+
+@Component({
+  selector: 'app-customers',
+  templateUrl: './customers.component.html',
+  styleUrls: ['./customers.component.css']
+})
+export class CustomersComponent implements OnInit {
+
+  constructor(
+    private customerService: CustomersService,
+    private fb: FormBuilder
+  ) {
+  }
+
+  public customers$!: Observable<Array<Customer>>; /* par convention, les variables contenant les observables se terminent par un $
+  ici on laisse le fichier HTML gérer le côté async en assignant a customers$ le resultat de l'observable*/
+  public errorMessage!: any;
+  public formGroup!: FormGroup;
+  private customer!: Customer;
+
+  ngOnInit(): void {
+
+    this.formGroup = this.fb.group({
+        keyword: this.fb.control("")
+      }
+    );
+
+    this.handleSearchCustomers();
+
+  }
+
+  public handleSearchCustomers() {
+    const kw = this.formGroup.value.keyword;
+    this.customers$ = this.customerService.searchCustomers(kw).pipe(
+      catchError(err => {
+        this.errorMessage = err.name;
+        return throwError(err);
+      })
+    );
+  }
+
+  public handleDeleteCustomer(customer: Customer) {
+    if (confirm("Êtes-vous sûr de vouloir supprimer " + customer.name + " ?")) {
+      this.customerService.deleteCustomer(customer.id).subscribe({
+        next: () => {
+          alert("le client à bien été supprimé");
+          this.customers$ = this.customers$.pipe(
+            map(data => {
+              const index = data.indexOf(customer);
+              data.slice(index, 1);
+              return data;
+            })
+          );
+        }, error: err => {
+          alert("Une erreur est survenue lors la suppression. " + err.message);
+          console.log('err', err);
+        }
+      });
+    } else return;
+
+
+  }
+
+}
