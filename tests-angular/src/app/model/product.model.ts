f063413be0f9a8453ba461ba2f97325f53f31db6
Alexandre <alexandre.lourencinho@gmail.com>
2022-06-25 16:31:46 +0200

    fixes

--- tests-angular/src/app/model/product.model.ts
+++ tests-angular/src/app/model/product.model.ts
@@ -1,13 +1,13 @@
 export interface Product {
-  id: string,
-  name: string,
-  price: number,
-  promotion: boolean
+  id: string;
+  name: string;
+  price: number;
+  promotion: boolean;
 }
 
 export interface PageProduct {
-  products: Array<Product>,
-  page:  number,
-  size: number,
-  totalPages: number
+  products: Array<Product>;
+  page:  number;
+  size: number;
+  totalPages: number;
 }
